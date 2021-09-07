06ac86959d2f77d2087a2588afff0afccaae4dde
tchaton <thomas@grid.ai>
2021-09-07 10:18:32 +0100

    i

--- cmd.sh
+++ cmd.sh
@@ -9,6 +9,7 @@ git fetch --all
 git checkout tpu_multi_pod
 git pull origin tpu_multi_pod
 sudo python3 -m pip install -e .
+pip install pytest
 #python3 -m pip install -r requirements.txt
 #python3 -m pip install -r requirements/test.txt
 python3 -m pytest tests/models/test_tpu.py::test_tpu_multi_pod
