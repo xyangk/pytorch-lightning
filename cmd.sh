d26a2499c1f13dc8bbc60a1f9351cd1e5d3af72a
tchaton <thomas@grid.ai>
2021-09-07 10:08:29 +0100

    i

--- cmd.sh
+++ cmd.sh
@@ -4,5 +4,7 @@ cd pytorch-lightning
 git fetch --all
 git checkout tpu_multi_pod
 git pull origin tpu_multi_pod
-python3 -m pip install -e '.[devel]'
+python3 -m pip install -e .
+python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements/test.txt
 python3 -m pytest tests/models/test_tpu.py::test_tpu_multi_pod
