7b706893fdcb18ed1f1a0a97ccde144823cdecb6
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-01-03 14:34:05 +0100

    x

--- pl_examples/bug_report/bug_report_model.py
+++ pl_examples/bug_report/bug_report_model.py
@@ -1,12 +1,14 @@
 import os
-
 import torch
-from torch.utils.data import DataLoader, Dataset
-
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
 from pytorch_lightning import LightningModule, Trainer
-
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.plugins import DeepSpeedPlugin
+from transformers.optimization import get_scheduler
 
 class RandomDataset(Dataset):
+
     def __init__(self, size, length):
         self.len = length
         self.data = torch.randn(length, size)
@@ -19,6 +21,7 @@ class RandomDataset(Dataset):
 
 
 class BoringModel(LightningModule):
+
     def __init__(self):
         super().__init__()
         self.layer = torch.nn.Linear(32, 2)
@@ -26,41 +29,77 @@ class BoringModel(LightningModule):
     def forward(self, x):
         return self.layer(x)
 
+    def train_dataloader(self):
+        return DataLoader(RandomDataset(32, 64), batch_size=2)
+
     def training_step(self, batch, batch_idx):
         loss = self(batch).sum()
         self.log("train_loss", loss)
-        return {"loss": loss}
+        return loss
 
     def validation_step(self, batch, batch_idx):
         loss = self(batch).sum()
         self.log("valid_loss", loss)
+        return loss
 
     def test_step(self, batch, batch_idx):
         loss = self(batch).sum()
         self.log("test_loss", loss)
+        return loss
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
-
+        no_decay = ["bias"]
+        params_decay = [
+            p for n, p in self.named_parameters() if not any(nd in n for nd in no_decay)
+        ]
+        params_nodecay = [
+            p for n, p in self.named_parameters() if any(nd in n for nd in no_decay)
+        ]
+        optim_groups = [
+            {
+                "params": params_decay,
+                "weight_decay": 0.01,
+            },
+            {"params": params_nodecay, "weight_decay": 0.0},
+        ]
+        optimizer = torch.optim.SGD(optim_groups, lr=0.1)
+        scheduler = get_scheduler("linear", optimizer, num_warmup_steps=10, num_training_steps=100)
+        return ([optimizer], [{"scheduler": scheduler}])
 
 def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    train_data = DataLoader(RandomDataset(32, 64), batch_size=4)
+    val_data = DataLoader(RandomDataset(32, 64), batch_size=4)
+    test_data = DataLoader(RandomDataset(32, 64), batch_size=4)
 
     model = BoringModel()
+    checkpoint_callback = ModelCheckpoint(
+        dirpath='tests4/checkpoints',
+        save_last=True,
+        every_n_train_steps=5,
+    )
     trainer = Trainer(
         default_root_dir=os.getcwd(),
-        limit_train_batches=1,
+        gpus=2,
+        limit_train_batches=1.0,
         limit_val_batches=1,
-        limit_test_batches=1,
         num_sanity_val_steps=0,
-        max_epochs=1,
-        enable_model_summary=False,
+        precision=16,
+        accelerator='deepspeed',
+        max_steps=50,
+        log_every_n_steps=10,
+        plugins=[DeepSpeedPlugin(stage=2)],
+        weights_summary=None,
+        callbacks=[checkpoint_callback, pl.callbacks.LearningRateMonitor(logging_interval="step")],
+        #resume_from_checkpoint='tests4/checkpoints/epoch=1-step=49.ckpt',
     )
-    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
+    trainer.fit(model)
     trainer.test(model, dataloaders=test_data)
 
+    # test lr_scheduler state
+    model_state_path = "tests4/checkpoints/epoch=1-step=49.ckpt/global_step50/mp_rank_00_model_states.pt"
+    model_state = torch.load(model_state_path, map_location="cpu")
+    print(model_state["lr_scheduler"])
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     run()
