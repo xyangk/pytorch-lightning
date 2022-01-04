98deaf7fc57505aae9649f062864947a1fc864eb
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-01-04 09:19:10 +0100

    move

--- pytorch_lightning/plugins/__init__.py
+++ pytorch_lightning/plugins/__init__.py
@@ -32,7 +32,7 @@ from pytorch_lightning.plugins.training_type.sharded import DDPShardedPlugin
 from pytorch_lightning.plugins.training_type.sharded_spawn import DDPSpawnShardedPlugin
 from pytorch_lightning.plugins.training_type.single_device import SingleDevicePlugin
 from pytorch_lightning.plugins.training_type.single_tpu import SingleTPUPlugin
-from pytorch_lightning.plugins.training_type.tpu_spawn import TPUSpawnPlugin
+from pytorch_lightning.strategies.tpu_spawn import TPUSpawnPlugin
 from pytorch_lightning.plugins.training_type.training_type_plugin import TrainingTypePlugin
 
 PLUGIN = Union[TrainingTypePlugin, PrecisionPlugin, ClusterEnvironment, CheckpointIO]
