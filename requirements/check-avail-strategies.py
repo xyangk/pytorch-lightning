557f60a1b101436e239e3bfa12fba13144ba7ba2
Akihiro Nitta <nitta@akihironitta.com>
2022-03-18 05:04:33 +0900

    Skip horovod check

--- requirements/check-avail-strategies.py
+++ requirements/check-avail-strategies.py
@@ -3,5 +3,6 @@ import deepspeed  # noqa: F401
 import fairscale  # noqa: F401
 import horovod.torch
 
+# TODO(akihironitta): Uncomment once the horovod installation issue is resolved (#12314)
 # returns an error code
-assert horovod.torch.nccl_built()
+# assert horovod.torch.nccl_built()
